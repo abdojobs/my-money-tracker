682900e69433930000930f6494e9831438b89373
zorderelda@gmail.com <zorderelda@gmail.com@05f6c59a-26c7-98fa-8e13-5b01f94a6614>
2012-10-05 02:18:07 +0000

    - Initial Input - Stub for output to Excel

---
+++ trunk/Money Tracker/Money Tracker/ExcelFileForm.vb
@@ -0,0 +1,16 @@
+﻿'
+' Created by SharpDevelop.
+' User: Jeff Walsh
+' Date: 19/09/2012
+' Time: 9:43 PM
+' Jeff Walsh - Game Associated Programming®
+' 
+'
+Public Partial Class ExcelFileForm
+	Public Sub New()
+		Me.InitializeComponent()
+		
+		
+		
+	End Sub
+End Class
